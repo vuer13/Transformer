bdb608eb6ae9471b23d007032152652dc07032f2
Ryan Vu <ryanvu1131@gmail.com>
2026-06-09 10:24:04 -0700

    feat: completed self attention head implementation

--- src/transformer/__init__.py
+++ src/transformer/__init__.py
@@ -1,10 +1,12 @@
 from transformer.config import Config
 from transformer.data import TextDatasetConfig, TextTokenDataset
 from transformer.bigram import BigramLanguageModel
+from transformer.attention import SelfAttentionHead
 
 __all__ = [
     "Config", 
     "TextDatasetConfig", 
     "TextTokenDataset", 
-    "BigramLanguageModel"
+    "BigramLanguageModel",
+    "SelfAttentionHead",
 ]
